62f5a672bc229f966ff37dac10b72827c6dea92f
Rob (local) <rob.m.kotz@gmail.com>
2016-09-30 15:26:57 -0700

    adding synchro_app submodule

---
+++ user-config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd /home/vagrant
+
+if [ ! -d /home/vagrant/synchro_venv ]; then
+  virtualenv synchro_venv
+fi
+
+# Put a nice vimrc in place
+cp /vagrant/configs/vimrc /home/vagrant/.vimrc
+
+# Put a .bashrc in place
+cp /vagrant/configs/bashrc /home/vagrant/.bashrc
+
+source /home/vagrant/synchro_venv/bin/activate
+# install pip packages
+cd /vagrant/synchro_app && pip install -r requirements.txt
+
+
+
+
