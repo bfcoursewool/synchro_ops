4ccc7298cfc9660f7d81eb0fe89c0813c00a54ef
Rob (local) <rob.m.kotz@gmail.com>
2016-09-30 15:17:48 -0700

    first synchro_ops commit

---
+++ provisioning.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get -y install nodejs
+sudo npm install -g grunt-cli
+sudo apt-get -y install python-pip
+sudo pip install virtualenv
+
+su -c "/vagrant/user-setup.sh" vagrant
