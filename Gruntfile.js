af4581a543e384e631380cc94cdd21018c542b0d
Rob (local) <rob.m.kotz@gmail.com>
2016-10-04 13:25:23 -0700

    updated gitignore, properly intialized synchro_ops as a node project, and set up a gruntfile for bootstrap and our own sass files

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        files: {
+          'synchro_app/css/carousel.css': 'synchro_app/css/carousel.scss'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default',['watch']);
+
+  // Run bootstrap's Gruntfile too
+  grunt.registerTask('run-grunt', function() {
+  var cb = this.async();
+  var child = grunt.util.spawn({
+      grunt: true,
+      args: ['clean', 'copy:fonts'],
+      opts: {
+          cwd: 'synchro_app/bootstrap-3.3.7'
+      }
+  }, function(error, result, code) {
+      cb();
+  });
+
+  child.stdout.pipe(process.stdout);
+  child.stderr.pipe(process.stderr);
+});
+
+}
+
