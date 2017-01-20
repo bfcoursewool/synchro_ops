91389895ab02b822e1ccc9cd709bc1ab5007d8b5
Rob (local) <rob.m.kotz@gmail.com>
2017-01-20 15:52:40 +0530

    sudo install the package maybe...

--- GCP/instance_startup_script.sh
+++ GCP/instance_startup_script.sh
@@ -3,6 +3,11 @@
 ## TODO -- Surely there is a way to do this with Chef or Ansible which would be better. 
 ## Once I have some time for the learning curve, we should use that method. 
 
+## These are now needed for all the PIP requirements to install, but
+## I'm too lazy to rebuild the base image yet so we'll do it here.
+sudo apt-get -y install libmysqlclient-dev
+sudo apt-get -y install libpython-dev
+
 # Grab the Synchro Platform from the app repo
 cd /var/www/html
 git clone https://github.com/bfcoursewool/synchro_ops.git
@@ -10,14 +15,14 @@ cd synchro_ops
 git submodule init
 git submodule update
 cd synchro_app
-git checkout v1-22
+git checkout v2-2
 
 # Move apache configs from ops repo & restart apache
 mv /var/www/html/synchro_ops/GCP/000-default.conf /etc/apache2/sites-enabled
 
 # Install the platform
 cd /var/www/html/synchro_ops/synchro_app 
-python setup.py install
+sudo python setup.py install
 
 # Restart Apache
 /etc/init.d/apache2 restart
