144c122cd27b9ee91d78e4a01d082bb768b9ee8c
Rob Kotz <rob.m.kotz@gmail.com>
2020-06-12 18:09:12 -1000

    temp fix for deploy process

--- GCP/deploy.sh
+++ GCP/deploy.sh
@@ -47,22 +47,23 @@ popd
 
 # Make an instance template w/ Startup Script that points to the tag to be deployed
 printf "${RED}++++++++++++++++++++++++++++ CREATE INSTANCE TEMPLATE +++++++++++++++++++++++++++++++++++++++${NC}\n"
-gcloud compute instance-templates create synchro-deploy-$TAG \
+gcloud compute --project "synchro-platform" instance-templates create synchro-deploy-$TAG \
 --image synchro-platform-v2-image \
+--image-project synchro-platform \
 --metadata-from-file startup-script="instance_startup_script.sh"
 
 # Make 2 new IGs from the IT we just made (Blue IG)
 # Group A
 printf "${RED}++++++++++++++++++++++++++++ CREATE INSTANCE GROUP A ++++++++++++++++++++++++++++++++++++++++${NC}\n"
 gcloud compute --project "synchro-platform" instance-groups managed create "instance-group-$TAG-a" --zone "us-central1-f" --base-instance-name "instance-group-$TAG-a" --template "synchro-deploy-$TAG" --size "1"
-gcloud compute --project "synchro-platform" instance-groups managed set-autoscaling "instance-group-$TAG-a" --zone "us-central1-f" --cool-down-period "60" --max-num-replicas "10" --min-num-replicas "1" --target-load-balancing-utilization "0.8"
-gcloud beta compute instance-groups managed set-autohealing "instance-group-$TAG-a" --initial-delay="300" --http-health-check="instance-group-1-hc" --zone="us-central1-f"
+#gcloud compute --project "synchro-platform" instance-groups managed set-autoscaling "instance-group-$TAG-a" --zone "us-central1-f" --cool-down-period "60" --max-num-replicas "10" --min-num-replicas "1" --target-load-balancing-utilization "0.8"
+#gcloud beta compute instance-groups managed set-autohealing "instance-group-$TAG-a" --initial-delay="300" --http-health-check="instance-group-1-hc" --zone="us-central1-f"
 
 # Group B
 printf "${RED}+++++++++++++++++++++++++++ CREATE INSTANCE GROUP B +++++++++++++++++++++++++++++++++++++++++${NC}\n"
 gcloud compute --project "synchro-platform" instance-groups managed create "instance-group-$TAG-b" --zone "us-east1-c" --base-instance-name "instance-group-$TAG-b" --template "synchro-deploy-$TAG" --size "1"
-gcloud compute --project "synchro-platform" instance-groups managed set-autoscaling "instance-group-$TAG-b" --zone "us-east1-c" --cool-down-period "60" --max-num-replicas "10" --min-num-replicas "1" --target-load-balancing-utilization "0.8"
-gcloud beta compute instance-groups managed set-autohealing "instance-group-$TAG-b" --initial-delay="300" --http-health-check="instance-group-1-hc" --zone="us-east1-c"
+#gcloud compute --project "synchro-platform" instance-groups managed set-autoscaling "instance-group-$TAG-b" --zone "us-east1-c" --cool-down-period "60" --max-num-replicas "10" --min-num-replicas "1" --target-load-balancing-utilization "0.8"
+#gcloud beta compute instance-groups managed set-autohealing "instance-group-$TAG-b" --initial-delay="300" --http-health-check="instance-group-1-hc" --zone="us-east1-c"
 
 # Wait for instance groups to be ready
 printf "${RED}+++++++++++++++++++++++++++ WAIT FOR INSTANCE GROUPS TO BE READY ++++++++++++++++++++++++++++${NC}\n"
@@ -71,14 +72,14 @@ sleep 30
 # Add the new IGs as backends to the synchro-backend-service
 # Group A
 printf "${RED}+++++++++++++++++++++++++++ ADD GROUP A TO BACKEND SERVICE ++++++++++++++++++++++++++++++++++${NC}\n"
-gcloud compute backend-services add-backend synchro-backend-service --global --instance-group="instance-group-$TAG-a" --balancing-mode="RATE" --max-rate-per-instance="80" --capacity-scaler="1.0" --instance-group-zone="us-central1-f"
+gcloud compute --project "synchro-platform" backend-services add-backend synchro-backend-service --global --instance-group="instance-group-$TAG-a" --balancing-mode="RATE" --max-rate-per-instance="80" --capacity-scaler="1.0" --instance-group-zone="us-central1-f"
 
 printf "${RED}+++++++++++++++++++++++++++ ADD GROUP B TO BACKEND SERVICE ++++++++++++++++++++++++++++++++++${NC}\n"
-gcloud compute backend-services add-backend synchro-backend-service --global --instance-group="instance-group-$TAG-b" --balancing-mode="RATE" --max-rate-per-instance="80" --capacity-scaler="1.0" --instance-group-zone="us-east1-c"
+gcloud compute --project "synchro-platform" backend-services add-backend synchro-backend-service --global --instance-group="instance-group-$TAG-b" --balancing-mode="RATE" --max-rate-per-instance="80" --capacity-scaler="1.0" --instance-group-zone="us-east1-c"
 
 # Invalidate Cloud CDN cache
 printf "${RED}+++++++++++++++++++++++++++ INVALIDATING LOAD BALANCER CACHE ++++++++++++++++++++++++++++++++++${NC}\n"
-gcloud compute url-maps invalidate-cdn-cache synchro-load-balancer --path="/*"
+gcloud compute --project "synchro-platform" url-maps invalidate-cdn-cache synchro-load-balancer --path="/*"
 
 
 # Remove the old (Green IG) from backend service & delete it
